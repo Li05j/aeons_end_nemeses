922b2ade14e79ff8608477d8f8d619f1af91bfa1
Jason <j989li@uwaterloo.ca>
2025-06-27 08:23:56 -0400

    random + next turn

--- src/lib/components/nemeses/rageborne/rageborne_view.svelte
+++ src/lib/components/nemeses/rageborne/rageborne_view.svelte
@@ -14,6 +14,9 @@
     let t3_deck: NemesisCard[] = [];
 
     let resolved_deck: NemesisCard[] = [];
+
+    let combined_deck: NemesisCard[] = [...t1_deck, ...t2_deck, ...t3_deck];
+    let current_card: NemesisCard | undefined = undefined;
   
     onMount(() => {
         const shuffled_t1_cards = shuffle_array($common_t1_nemesis_cards);
@@ -22,19 +25,30 @@
         t2_deck = shuffled_t2_cards.slice(0, TOTAL_T2_CARDS);
         const shuffled_t3_cards = shuffle_array($common_t3_nemesis_cards);
         t3_deck = shuffled_t3_cards.slice(0, TOTAL_T3_CARDS);
+
+        combined_deck = [...t1_deck, ...t2_deck, ...t3_deck];
+        current_card = combined_deck.shift() || undefined;
     });
+    
+    function next_turn() {
+        if (current_card) {
+            resolved_deck.push(current_card);
+            current_card = combined_deck.shift();
+        }
+    }
+
 </script>
 
 <div class="flex flex-col items-center justify-center h-full">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {#each t1_deck as card}
-            <NemesisCardComponent card_data={card}/>
-        {/each}
-        {#each t2_deck as card}
-            <NemesisCardComponent card_data={card}/>
-        {/each}
-        {#each t3_deck as card}
-            <NemesisCardComponent card_data={card}/>
-        {/each}
+        <NemesisCardComponent card_data={current_card}/>
     </div>
+
+    <button 
+        class="absolute bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded"
+        on:click={() => next_turn()}
+        disabled={!current_card}
+    >
+        Next Turn
+    </button>
 </div>
--- src/lib/components/nemesis_card_component.svelte
+++ src/lib/components/nemesis_card_component.svelte
@@ -2,61 +2,67 @@
     import { onMount } from 'svelte';
     import type { NemesisCard } from '$lib/interface';
 
-    export let card_data: NemesisCard;
+    export let card_data: NemesisCard | undefined;
 
     let power_left: number = -1;
     let minion_hp: number = -1;
 
-    let card_bg_color: string = 'bg-white';
+    $: card_bg_color = set_bg_color(card_data);
 
-    onMount(() => {
+    function set_bg_color(card_data: NemesisCard | undefined) {
+        if (!card_data) {
+            return 'bg-white';
+        }
+
         const type = card_data.type;
         switch (type) {
             case 'attack':
-                card_bg_color = 'bg-purple-100';
-                break;
+                return 'bg-purple-100';
             case 'power':
                 power_left = card_data.power;
-                card_bg_color = 'bg-yellow-100';
-                break;
+                return 'bg-yellow-100';
             case 'minion':
                 minion_hp = card_data.health;
-                card_bg_color = 'bg-blue-100';
-                break;
+                return 'bg-blue-100';
             default:
-                card_bg_color = 'bg-white';
-                break;
+                return 'bg-white';
         }
-    });
+    }
+
 </script>
 
 <div class="{card_bg_color} p-4 space-y-4 card-hover overflow-hidden max-w-xs">
-    <header class="pb-0">
-        <h6 class="text-lg font-bold text-black text-center my-0">{card_data.title}</h6>
-    </header>
-    
-    <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
-    
-    <div>
-        <p class="whitespace-pre-line pb-4">{card_data.description}</p>
-        
-        <div class="space-y-2">
-            {#if card_data.type === 'attack'}
-                <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
-                <p class="text-sm text-right">Tier: {card_data.tier}</p>
-            {:else if card_data.type === 'power'}
-                <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
-                <div class="flex justify-between items-center">
-                    <p class="text-sm">Power: {power_left}</p>
+    {#if card_data === undefined}
+        <p class="text-center text-gray-500">No more cards</p>
+        <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
+    {:else}
+        <header class="pb-0">
+            <h6 class="text-lg font-bold text-black text-center my-0">{card_data.title}</h6>
+        </header>
+
+        <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
+
+        <div>
+            <p class="whitespace-pre-line pb-4">{card_data.description}</p>
+
+            <div class="space-y-2">
+                {#if card_data.type === 'attack'}
+                    <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
                     <p class="text-sm text-right">Tier: {card_data.tier}</p>
-                </div>
-            {:else if card_data.type === 'minion'}
-                <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
-                <div class="flex justify-between items-center">
-                    <p class="text-sm">Health: {minion_hp}</p>
-                    <p class="text-sm text-right">Tier: {card_data.tier}</p>
-                </div>
-            {/if}
+                {:else if card_data.type === 'power'}
+                    <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
+                    <div class="flex justify-between items-center">
+                        <p class="text-sm">Power: {power_left}</p>
+                        <p class="text-sm text-right">Tier: {card_data.tier}</p>
+                    </div>
+                {:else if card_data.type === 'minion'}
+                    <hr class="h-1 bg-black border-0 opacity-75 mt-0" />
+                    <div class="flex justify-between items-center">
+                        <p class="text-sm">Health: {minion_hp}</p>
+                        <p class="text-sm text-right">Tier: {card_data.tier}</p>
+                    </div>
+                {/if}
+            </div>
         </div>
-    </div>
+    {/if}
 </div>
